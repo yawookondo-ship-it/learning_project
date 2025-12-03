93dab8b0de1849d11dfba6a67278cc8f6aae5793
yawookondo-ship-it <yawookondo@gmail.com>
2025-12-02 22:14:31 -0500

    vpc subnet formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,17 @@
-resource "aws_s3_bucket" "yawo_obi_12" {
-  bucket = "yawo_obi_12" # Bucket names must be globally unique
+resource "aws_s3_bucket" "yawo" {
+  bucket = "yawo_obi_12"
 
   tags = {
-    Project     = "yawo_obi_12"
+    Name        = "yawo"
     Environment = "Dev"
   }
 }
 
-module "ec2_instance" {
-  source        = "./ec2/aws"
-  #version       = "5.0" # Use the latest version for new projects
-
-  name          = "Training_1"
-  instance_type = "t3.micro"
-  ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
-  subnet_id     = "10.0.1.0/24"      # Replace with your specific subnet ID
-  monitoring    = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+# Create a new host with instance type of c5.18xlarge with Auto Placement
+# and Host Recovery enabled.
+resource "aws_ec2_host" "test" {
+  instance_type     = "t3.micro"
+  availability_zone = "us-east-1"
+  host_recovery     = "off"
+  auto_placement    = "off"
 }
